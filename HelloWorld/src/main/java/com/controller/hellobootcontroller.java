a0851e375b0dd9fa24e657303f84c80ec6834da4
Vishakha <Vishakha@Vishakha-PC.mshome.net>
2021-06-04 00:02:16 +0530

    added mapping

--- HelloWorld/src/main/java/com/controller/hellobootcontroller.java
+++ HelloWorld/src/main/java/com/controller/hellobootcontroller.java
@@ -1,10 +1,10 @@
 package com.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class hellobootcontroller {
-	@RequestMapping(value="/")
+	@GetMapping("/testhelloservice")
 	public String hello()
 	{
 		return "Hello World";
